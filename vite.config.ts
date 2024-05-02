6074dc1674e34a4695b52c05194f69438ed6b615
Nigel Ashiono <nigel.ashiono@sicon.co.uk>
2024-05-02 11:23:16 +0100

    Removed security leak

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   root: 'src',
+//   build: {
+//     outDir: 'dist',
+
+//     lib: {
+//       entry: resolve(__dirname, 'src/index.ts'),
+//       name: 'dist',
+//       fileName: (format) => `index.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ['react', 'react-dom', '@emotion/react', '@emotion/styled'],
+//       output: {
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//           '@emotion/react': 'emotionReact',
+//           '@emotion/styled': 'emotionStyled',
+//         },
+//       },
+//     },
+//   },
+// })
+
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'Storybook Test',
+      formats: ['es', 'umd'],
+      fileName: (format) => `sicon-dev-storybook-test.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '@emotion/styled'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@emotion/styled': 'styled',
+        },
+      },
+    },
+  },
 })
